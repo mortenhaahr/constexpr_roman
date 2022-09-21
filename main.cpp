26077a2589c2fb419871dc738c0374e838b6a4af
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2022-09-21 21:19:25 +0200

    Working in boost

--- main.cpp
+++ main.cpp
@@ -1,75 +1,48 @@
-#include <string>
-#include <utility>
-#include <array>
+#include <boost/hana.hpp>
 
-template<const std::string_view &... Strs>
-struct cexpr_string_join {
-  // Join all strings into a single std::array of chars
-  static constexpr auto impl() noexcept {
-    constexpr std::size_t len = (Strs.size() + ... + 0);
-    std::array<char, len + 1> arr{};
-    auto append = [i = 0, &arr](auto const &s) mutable {
-      for (auto c : s) arr[i++] = c;
-    };
-    (append(Strs), ...);
-    arr[len] = 0;
-    return arr;
-  }
-  // Give the joined string static storage
-  static constexpr auto arr = impl();
-  // View as a std::string_view
-  static constexpr std::string_view value{arr.data(), arr.size() - 1};
-};
+using namespace boost;
+using namespace hana::literals;
 
-constexpr std::array<const std::pair<const std::string_view, int>, 13> lookupTable{{
-                                                                                       {"M", 1000},
-                                                                                       {"CM", 900},
-                                                                                       {"D", 500},
-                                                                                       {"CD", 400},
-                                                                                       {"C", 100},
-                                                                                       {"XC", 90},
-                                                                                       {"L", 50},
-                                                                                       {"XL", 40},
-                                                                                       {"X", 10},
-                                                                                       {"IX", 9},
-                                                                                       {"V", 5},
-                                                                                       {"IV", 4},
-                                                                                       {"I", 1}
-                                                                                   }};
+constexpr auto lookupTable = boost::hana::make_tuple(
+    boost::hana::make_pair(BOOST_HANA_STRING("M"), 1000),
+    boost::hana::make_pair(BOOST_HANA_STRING("CM"), 900),
+    boost::hana::make_pair(BOOST_HANA_STRING("D"), 500),
+    boost::hana::make_pair(BOOST_HANA_STRING("CD"), 400),
+    boost::hana::make_pair(BOOST_HANA_STRING("C"), 100),
+    boost::hana::make_pair(BOOST_HANA_STRING("XC"), 90),
+    boost::hana::make_pair(BOOST_HANA_STRING("L"), 50),
+    boost::hana::make_pair(BOOST_HANA_STRING("XL"), 40),
+    boost::hana::make_pair(BOOST_HANA_STRING("X"), 10),
+    boost::hana::make_pair(BOOST_HANA_STRING("IX"), 9),
+    boost::hana::make_pair(BOOST_HANA_STRING("V"), 5),
+    boost::hana::make_pair(BOOST_HANA_STRING("IV"), 4),
+    boost::hana::make_pair(BOOST_HANA_STRING("I"), 1)
+);
 
+template<int remainder, int numeralIndex = 0>
+static constexpr auto toRoman() {
+  // Weird Hana syntax for retrieving tuple elements...
+  constexpr auto first = hana::first(lookupTable[hana::size_t<numeralIndex>{}]);
+  constexpr auto second = hana::second(lookupTable[hana::size_t<numeralIndex>{}]);
 
-
-template<int remainder, int numeralIndex, const std::string_view & roman>
-static constexpr std::string_view toRomanRec() {
   if constexpr(remainder == 0) {
-    return roman;
+    return BOOST_HANA_STRING("");
+  } else if constexpr(remainder >= second) {
+    return first + toRoman<remainder - second, numeralIndex>();
+  } else {
+    return toRoman<remainder, numeralIndex + 1>();
   }
-  constexpr auto p = lookupTable[numeralIndex];
-  if constexpr(remainder >= p.second) {
-    return toRomanRec<remainder - p.second, numeralIndex, cexpr_string_join<roman, lookupTable[numeralIndex].first>::value>();
-  }
-//  else {
-//    return toRomanRec<remainder, numeralIndex + 1, roman>();
-//  }
-  return roman;
 }
 
-static constexpr std::string_view str = "";
-template<int number>
-static constexpr std::string_view solution() {
-  return toRomanRec<number, 0, str>();
-//  auto x =  toRomanRec(number, 0, "");
-//  return x;
-}
-
-template<int i>
-static constexpr auto test(){
-  return lookupTable[i].first;
-}
-
+#define PRINT
+#ifdef PRINT
 #include <iostream>
+#endif
+
 int main() {
-  auto x = solution<1000>();
-  std::cout << x << std::endl;
-  auto y = 2;
+  auto x = toRoman<2022>();
+  static_assert(x == BOOST_HANA_STRING("MMXXII"), "Not compile-time");
+  #ifdef PRINT
+  std::cout << x.c_str() << std::endl;
+  #endif
 }
